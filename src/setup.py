92adafb5d9495d47a4f1745bef7c1738ef1caf97
char46 <haosiru@h-sr.cn>
2024-02-08 01:46:37 +0800

    publish

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="PySemver",
+    name="Py-Semver-c",
     version="0.1.0",
     description="A python package for semver version",
     author="char46",
     author_email="haosiru@h-sr.cn",
     license="MIT",
-    packages=["semver"],
+    packages=["PySemver"],
     install_requires=["typing_extensions"],
     python_requires=">=3.8",
     url=r"https://github.com/char-46/PySemver",
